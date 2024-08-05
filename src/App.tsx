e0e4e9652f7db41f4a59f9c499f78a36a2a1e0fe
Leroy Saldanha <leroyds@live.com>
2024-08-05 16:00:42 +0530

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,56 @@ import { Provider } from 'react-redux'
 import './App.css'
 import store from './utils/redux/appStore.ts';
 import Home from './components/Home/Home.tsx';
-
+import Header from './components/Header/Header.tsx';
+import { Suspense } from 'react';
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import SignUp from './components/SignUp/SignUp.tsx';
+import Admin from './components/Admin/Admin.tsx';
+import User from './components/User/User.tsx';
+import './components/Home/home.scss';
 function App() {
 
 
+  const AppLayout = () => {
+    return (
+      <Provider store={store}>
+        <div>
+            <Header />
+            <div className="container">
+            <Suspense fallback={<div>Loading...</div>}>
+              <Outlet/>
+            </Suspense>
+            </div>
+        </div>
+      </Provider>
+    );
+  };
+
+
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+          {
+            path: "/",
+            element: <SignUp/>,
+          },
+          {
+            path: "/admin",
+            element: <Admin />,
+          },
+          {
+            path: "/user",
+            element: <User />,
+          },
+      ]
+    },
+  ]);
 
 
   return (
-    <Provider store={store}>
-      <Home/>
-    </Provider>
+    <RouterProvider router={appRouter} />
   )
 }
 
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,40 +1,80 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../utils/redux/appStore";
-import { updateUser } from "../../utils/redux/slice/userSlice";
-import Admin from "../Admin/Admin";
-import Header from "../Header/Header";
-import './home.scss';
-import { useSelector } from "react-redux";
-import User from "../User/User";
+// import { useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { AppDispatch } from "../../utils/redux/appStore";
+// import { updateUser } from "../../utils/redux/slice/userSlice";
+// import Admin from "../Admin/Admin";
+// import Header from "../Header/Header";
 
-const Home = () => {
-  const dispatch: AppDispatch = useDispatch();
-  const user = useSelector((state) => state.user);
+// import { useSelector } from "react-redux";
+// import User from "../User/User";
+// import SignUp from "../SignUp/SignUp";
+
+// const Home = () => {
+//   const dispatch: AppDispatch = useDispatch();
+//   const user = useSelector((state) => state.user);
 
 
-  useEffect(() => {
-    fetchAndSetUser()
-  }, [])
+//   useEffect(() => {
+//     fetchAndSetUser()
+//   }, [])
 
-  async function fetchAndSetUser(){
-    const response = await fetch('https://f2ed36a4mh.execute-api.ap-south-1.amazonaws.com/');
-    const json = await response.json();
-    dispatch(updateUser(json));
-  }
+//   async function fetchAndSetUser(){
+//     const response = await fetch('https://f2ed36a4mh.execute-api.ap-south-1.amazonaws.com/');
+//     const json = await response.json();
+//     dispatch(updateUser(json));
+//   }
 
-  return (
-    <div>
-      <Header fetchAndSetUser={fetchAndSetUser}/>
-      <div className="container">
-        {
-          user.role === 'admin' ?
-            <Admin/>
-            :<User/>
-        }
-      </div>
-    </div>
-  );
-}
+//   const AppLayout = () => {
+//     return (
+//       <Provider store={appStore}>
+//         <div className="app">
+//             <Header />
+//             <Suspense fallback={<div>Loading...</div>}>
+//               <Outlet/>
+//             </Suspense>
+//         </div>
+//       </Provider>
+//     );
+//   };
+
+//   const appRouter = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <SignUp />,
+//       children: [
+//           {
+//             path: "/",
+//             element: <Home/>,
+//           },
+//           {
+//             path: "/about",
+//             element: <About />,
+//           },
+//           {
+//             path: "/contact",
+//             element: <Contact />,
+//           },
+//           {
+//             path: "/restaurant/:restId",
+//             element: <RestaurantMenu/>
+//           }
+//       ],
+//       errorElement: <Error />,
+//     },
+//   ]);
+
+//   return (
+//     <div>
+//       <Header fetchAndSetUser={fetchAndSetUser}/>
+//       <div className="container">
+//         {
+//           user.role === 'admin' ?
+//             <Admin/>
+//             :<User/>
+//         }
+//       </div>
+//     </div>
+//   );
+// }
  
-export default Home;
+// export default Home;
